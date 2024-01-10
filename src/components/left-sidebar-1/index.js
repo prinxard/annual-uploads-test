5f958005d337e88a4ec215f3205910f542442e32
prinxard <prinxard@gmail.com>
2024-01-10 21:20:37 +0100

    update

--- src/components/left-sidebar-1/index.js
+++ src/components/left-sidebar-1/index.js
@@ -38,11 +38,11 @@ const LeftSidebar = () => {
   );
 
   let approverRange = [1, 2, 3, 12, 21, 27, 20]
+  let payeTccApprover = [1, 30]
   let creatorRange = [1, 4, 13, 15]
   let adminRange = [1]
   let reportRange = [39, 1, 9]
   let payeTccInitiator = [29, 1]
-  let payeTccApprover = [1, 30, 21, 30]
   let auditPrint = [1, 42, 19]
   let audit = [1, 19]
   let otherTaxes = [1, 24]
